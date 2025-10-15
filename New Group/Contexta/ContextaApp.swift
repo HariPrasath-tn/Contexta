af7675a0cfa15ccc086b8d38eff165272e42bd83
hari <hariprasath.dp@zohocorp.com>
2025-10-15 12:18:28 +0530

    added basic structure

---
+++ New Group/Contexta/ContextaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContextaApp.swift
+//  Contexta
+//
+//  Created by Hari on 13/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ContextaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
